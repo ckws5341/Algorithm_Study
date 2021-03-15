25b56bfcdf43984b8f41751aaafec94b11a91e95
Taesung <teamwkdwkd@gmail.com>
2021-03-15 21:27:00 +0900

    Start Algorithm_Study

---
+++ Algorithm_Study/AlgoritmStudy_March_Week2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+#define ALGORITHM_1099
+//#define ALGORITHM_1095
+//#define ALGORITHM_1060
+
+int main()
+{
+#pragma region 1099¹ø
+#ifdef ALGORITHM_1099
+#endif
+#pragma endregion
+
+#pragma region 1095¹ø
+#ifdef ALGORITHM_1095
+#endif
+#pragma endregion
+
+#pragma region 1060¹ø
+#ifdef ALGORITHM_1060
+#endif
+#pragma endregion
+	return 0;
+}
